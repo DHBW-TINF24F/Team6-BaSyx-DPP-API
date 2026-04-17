5d5d1b43871108e5861e01fc3d27b2fc46e6a076
Odiplus <snips8472@gmail.com>
2026-04-17 15:35:01 +0200

    added script to create dpps for testing
    Co-authored-by: lxca03 <lucaschmoll@gmail.com>

---
+++ EXECUTABLE/dpp-backend/create_dpp.sh
@@ -0,0 +1,29 @@
+printf "-----------------------------------\n"
+printf " Create a DPP\n"
+printf "-----------------------------------\n"
+
+curl -X POST http://localhost:8080/dpps \
+-H "Content-Type: application/json" \
+-d '{
+  "shell": {
+    "id": "urn:uuid:7e51f712-429a-419a-9e8c-8f43c393850b",
+    "dpps": [
+      {
+        "productId": "PID",
+        "version": "1.0.0",
+        "submodels": [
+          {
+            "name": "CarbonFootPrint",
+            "version": "1.2.0",
+            "reference": "urn:submodel:cfp:001"
+          },
+          {
+            "name": "DigitalNamePlate",
+            "version": "1.0.1",
+            "reference": "urn:submodel:dnp:002"
+          }
+        ]
+      }
+    ]
+  }
+}'
